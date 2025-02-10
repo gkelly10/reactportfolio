a6a2cad6954185fb235c4ee205d5151e2b49750e
gkelly10 <gabriellekelly10101@gmail.com>
2025-02-10 17:10:12 -0500

    add an h1 to aboutme

--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -2,7 +2,8 @@
 const AboutMe = () => {
   return (
     <section className="aboutMe">
-      <h1>A Boston-based Full-stack Software Engineer</h1>
+      <h1>Gabrielle Nascimento</h1>
+      <h2>A Boston-based Full-stack Software Engineer</h2>
       <p>
         I love creating eye catching, minimalist, and innovative websites that can elevate businesses. 
         When not coding, I like to hike, pick up new hobbies (so far I have learned how to play a few string instruments, embroidery, and sewing), and trying new restaurants in Boston, although, I love homecooked meals.
